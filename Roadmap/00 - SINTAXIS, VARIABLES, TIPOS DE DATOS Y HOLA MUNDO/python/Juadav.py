d06f71e972b30c56c90f8a318d3bdd130b5d2d92
Juan David Alvarado Cornejo <juanalvaradocor01@gmail.com>
2024-12-05 13:17:33 -0500

    Agregar mi primer reto

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Juadav.py
@@ -0,0 +1,17 @@
+# https://www.python.org/
+
+# Esto es un comentario es una linea
+"""
+Comentarios de 
+multiples lineas.
+"""
+
+variable = "Una variable"
+
+
+String = "cadena de texto" 
+int = 1
+float = 3.14
+boolean = True
+
+print("¡Hola, Python!")
